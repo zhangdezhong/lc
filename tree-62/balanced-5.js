af6f09e50225a26a3e7772bd496ad9037cef2374
zhangdezhong <zhang.dezhong@immomo.com>
2021-01-26 01:09:11 +0800

    ok

--- tree-62/balanced-5.js
+++ tree-62/balanced-5.js
@@ -14,21 +14,49 @@ var isBalanced = function(root) {
     
     return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right);
 };
-
+var isBalanced = function(root) {
+    function dfsHeight (root) {
+        if (root == null) return 0;
+        let leftHeight = dfsHeight(root.left);
+        if (leftHeight == -1) return -1;
+        let rightHeight = dfsHeight (root.right);
+        if (rightHeight == -1) return -1;
+        if (Math.abs(leftHeight - rightHeight) > 1)  return -1;
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+    return dfsHeight (root) != -1;
+}
 // 101. 对称二叉树
 // https://leetcode-cn.com/problems/symmetric-tree/
 // 剑指 Offer 28. 对称的二叉树
 // https://leetcode-cn.com/problems/dui-cheng-de-er-cha-shu-lcof/
 var isSymmetric = function(root) {
     function isSymmetricHelp(left, right){
-        if(!left || !right)
+        if(left == null || right == null) {
             return left==right;
-        if(left.val != right.val)
+        }
+        if(left.val != right.val) {
             return false;
+        }
         return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.left);
     }
     return root==null || isSymmetricHelp(root.left, root.right);
 };
+function isSymmetric(root) {
+    if (root == null) return true;
+    let stack = [root.left, root.right];
+    while (stack.length) {
+        let right = stack.pop();
+        let left = stack.pop();
+        if (right == null && left == null) continue;
+        if (right == null || left == null || right.val != left.val) return false;
+        stack.push(right.left);
+        stack.push(left.right);
+        stack.push(right.right);
+        stack.push(left.left);
+    }
+    return true;
+}
 // 剑指 Offer 27. 二叉树的镜像
 // https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof/
 var mirrorTree = function(root) {
